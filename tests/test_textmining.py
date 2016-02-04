7d095cd52d7023fd4757bb7224fb1dc9acd9229e
Kevin Jacobs <kevin91nl@gmail.com>
2016-02-04 17:35:48 +0100

    Zip file handling implemented.

--- tests/test_textmining.py
+++ tests/test_textmining.py
@@ -1,10 +1,57 @@
 import unittest
+import shutil
 from textmining.extract import *
 
 
 class TestTools(unittest.TestCase):
-    def test_always_wrong(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.zipfile = 'testzip.zip'
+        self.badzipfile = 'testzip_bad.zip'
+        self.zip_contents = {
+            'testfile.txt': 'some text',
+            'testfile2.txt': 'some other text'
+        }
+        # Write the content to each file
+        for file in self.zip_contents:
+            with open(file, 'w') as handle:
+                handle.write(self.zip_contents[file])
+        # Create the zipfile
+        with zipfile.ZipFile(self.zipfile, 'w') as testzip:
+            for file in self.zip_contents:
+                testzip.write(file)
+        # Create the bad zipfile
+        with open(self.badzipfile, 'w') as handle:
+            handle.write('bad zip contents')
+        # Create an empty folder
+        if not os.path.exists('empty_folder'):
+            os.mkdir('empty_folder')
+
+    def tearDown(self):
+        shutil.rmtree('empty_folder')
+        files = list(self.zip_contents.keys())
+        files.extend([self.badzipfile, self.zipfile])
+        for file in files:
+            os.remove(file)
+
+    def handle_zip_file(self, path, text):
+        self.found_zip_contents[path] = text
+
+    def test_parse_zip_file(self):
+        self.found_zip_contents = {}
+        parse_zip_file(self.zipfile, self.handle_zip_file)
+        for file in self.found_zip_contents:
+            self.assertTrue(file in self.zip_contents)
+        for file in self.zip_contents:
+            self.assertTrue(file in self.found_zip_contents)
+            self.assertEqual(self.zip_contents[file], self.found_zip_contents[file])
+
+    def test_parse_zip_file_badzipfile(self):
+        with self.assertRaises(zipfile.BadZipfile):
+            parse_zip_file(self.badzipfile, self.handle_zip_file)
+        with self.assertRaises(zipfile.BadZipfile):
+            parse_zip_file('unknown_file', self.handle_zip_file)
+        with self.assertRaises(zipfile.BadZipfile):
+            parse_zip_file('empty_folder', self.handle_zip_file)
 
 
 if __name__ == '__main__':
--- textmining/extract.py
+++ textmining/extract.py
@@ -1,6 +1,8 @@
 """
 This file contains tools for handling text documents and extracting features from it.
 """
+import zipfile
+import os
 
 
 def parse_zip_file(path, handler):
@@ -14,11 +16,22 @@ def parse_zip_file(path, handler):
     handler: function
         When looping through all the files contained in the zip file, this method will be called every time
         a new file is found. Two arguments are passed. The first argument is the name of the discovered file
-        and the second argument are the contents of the file.
+        and the second argument is the UTF-8 text found in the document.
 
     Returns
     -------
     None
         Nothing is returned.
+
+    Raises
+    ------
+    zipfile.BadZipfile
+        If the given path is not a zip file.
     """
-    return None
+    if not os.path.exists(path) or os.path.isdir(path) or not zipfile.is_zipfile(path):
+        raise zipfile.BadZipfile
+    with zipfile.ZipFile(path, 'r') as zip:
+        for file in zip.namelist():
+            with zip.open(file, 'r') as file_handle:
+                text = file_handle.read().decode('utf-8')
+                handler(file, text)
