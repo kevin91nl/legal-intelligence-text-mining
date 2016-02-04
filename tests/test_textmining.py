d290d42cbe6b1cae0672da4266eb67726f2bec9d
Kevin Jacobs <kevin91nl@gmail.com>
2016-02-04 17:37:58 +0100

    Zip file handling implemented.

--- tests/test_textmining.py
+++ tests/test_textmining.py
@@ -5,6 +5,7 @@ from textmining.extract import *
 
 class TestTools(unittest.TestCase):
     def setUp(self):
+        # Set up some files
         self.zipfile = 'testzip.zip'
         self.badzipfile = 'testzip_bad.zip'
         self.zip_contents = {
@@ -27,6 +28,7 @@ class TestTools(unittest.TestCase):
             os.mkdir('empty_folder')
 
     def tearDown(self):
+        # Remove all of the created files
         shutil.rmtree('empty_folder')
         files = list(self.zip_contents.keys())
         files.extend([self.badzipfile, self.zipfile])
@@ -34,9 +36,12 @@ class TestTools(unittest.TestCase):
             os.remove(file)
 
     def handle_zip_file(self, path, text):
+        # Helper function for handling files within the zip file
         self.found_zip_contents[path] = text
 
     def test_parse_zip_file(self):
+        # Test whether it is really possible to parse a zip file (and check if the found contents are indeed the stored
+        # contents.
         self.found_zip_contents = {}
         parse_zip_file(self.zipfile, self.handle_zip_file)
         for file in self.found_zip_contents:
@@ -46,11 +51,15 @@ class TestTools(unittest.TestCase):
             self.assertEqual(self.zip_contents[file], self.found_zip_contents[file])
 
     def test_parse_zip_file_badzipfile(self):
+        # Test the exceptions for the zip file parsing
         with self.assertRaises(zipfile.BadZipfile):
+            # A zip file with invalid contents (is not a valid zip file)
             parse_zip_file(self.badzipfile, self.handle_zip_file)
         with self.assertRaises(zipfile.BadZipfile):
+            # A non-existing file (is not a valid zip file)
             parse_zip_file('unknown_file', self.handle_zip_file)
         with self.assertRaises(zipfile.BadZipfile):
+            # A directory (is not a valid zip file)
             parse_zip_file('empty_folder', self.handle_zip_file)
 
 
